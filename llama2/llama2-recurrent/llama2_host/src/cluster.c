0ef759f5ebe31fb61b3ba0314a1879f46c16b89a
tiagolascasas <tiagolascasas@outlook.com>
2025-01-03 03:56:15 +0000

    Partial llama2 version with a recurrent kernel

---
+++ llama2/llama2-recurrent/llama2_host/src/cluster.c
@@ -0,0 +1,488 @@
+#include <stdbool.h>
+#include <sys/types.h>
+
+typedef struct
+{
+    int dim;        // transformer dimension
+    int hidden_dim; // for ffn layers
+    int n_layers;   // number of layers
+    int n_heads;    // number of query heads
+    int n_kv_heads; // number of key/value heads (can be < query heads because of multiquery)
+    int vocab_size; // vocabulary size, usually 256 (byte-level)
+    int seq_len;    // max sequence length
+} Config;
+
+typedef struct
+{
+    // token embedding table
+    float *token_embedding_table; // (vocab_size, dim)
+    // weights for rmsnorms
+    float *rms_att_weight; // (layer, dim) rmsnorm weights
+    float *rms_ffn_weight; // (layer, dim)
+    // weights for matmuls. note dim == n_heads * head_size
+    float *wq; // (layer, dim, n_heads * head_size)
+    float *wk; // (layer, dim, n_kv_heads * head_size)
+    float *wv; // (layer, dim, n_kv_heads * head_size)
+    float *wo; // (layer, n_heads * head_size, dim)
+    // weights for ffn
+    float *w1; // (layer, hidden_dim, dim)
+    float *w2; // (layer, dim, hidden_dim)
+    float *w3; // (layer, hidden_dim, dim)
+    // final rmsnorm
+    float *rms_final_weight; // (dim,)
+    // (optional) classifier weights for the logits, on the last layer
+    float *wcls;
+} TransformerWeights;
+
+typedef struct
+{
+    // current wave of activations
+    float *x;      // activation at current time stamp (dim,)
+    float *xb;     // same, but inside a residual branch (dim,)
+    float *xb2;    // an additional buffer just for convenience (dim,)
+    float *hb;     // buffer for hidden dimension in the ffn (hidden_dim,)
+    float *hb2;    // buffer for hidden dimension in the ffn (hidden_dim,)
+    float *q;      // query (dim,)
+    float *k;      // key (dim,)
+    float *v;      // value (dim,)
+    float *att;    // buffer for scores/attention values (n_heads, seq_len)
+    float *logits; // output logits
+    // kv cache
+    float *key_cache;   // (layer, seq_len, dim)
+    float *value_cache; // (layer, seq_len, dim)
+} RunState;
+
+typedef struct
+{
+    Config config;              // the hyperparameters of the architecture (the blueprint)
+    TransformerWeights weights; // the weights of the model
+    RunState state;             // buffers for the "wave" of activations in the forward pass
+    // some more state needed to properly clean up the memory mapping (sigh)
+    int fd;            // file descriptor for memory mapping
+    float *data;       // memory mapped data pointer
+    ssize_t file_size; // size of the checkpoint file in bytes
+} Transformer;
+
+typedef struct
+{
+    char *str;
+    int id;
+} TokenIndex;
+
+typedef struct
+{
+    char **vocab;
+    float *vocab_scores;
+    TokenIndex *sorted_vocab;
+    int vocab_size;
+    unsigned int max_token_length;
+    unsigned char byte_pieces[512]; // stores all single-byte strings
+} Tokenizer;
+
+typedef struct
+{
+    float prob;
+    int index;
+} ProbIndex; // struct used when sorting probabilities during top-p sampling
+
+typedef struct
+{
+    int vocab_size;
+    ProbIndex *probindex; // buffer used in top-p sampling
+    float temperature;
+    float topp;
+    unsigned long long rng_state;
+} Sampler;
+
+static void rmsnorm(float *o, float *x, float *weight, int size)
+{
+    // calculate sum of squares
+    float ss = 0.0f;
+    for (int j = 0; j < size; j++)
+    {
+        ss += x[j] * x[j];
+    }
+    ss /= size;
+    ss += 1e-5f;
+    ss = 1.0f / sqrtf(ss);
+    // normalize and scale
+    for (int j = 0; j < size; j++)
+    {
+        o[j] = weight[j] * (ss * x[j]);
+    }
+}
+
+static void softmax(float *x, int size)
+{
+    // find max value (for numerical stability)
+    float max_val = x[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (x[i] > max_val)
+        {
+            max_val = x[i];
+        }
+    }
+    // exp and sum
+    float sum = 0.0f;
+    for (int i = 0; i < size; i++)
+    {
+        x[i] = expf(x[i] - max_val);
+        sum += x[i];
+    }
+    // normalize
+    for (int i = 0; i < size; i++)
+    {
+        x[i] /= sum;
+    }
+}
+
+static void matmul(float *xout, float *x, float *w, int n, int d)
+{
+    // W (d,n) @ x (n,) -> xout (d,)
+    // by far the most amount of time is spent inside this little function
+    int i;
+#pragma omp parallel for private(i)
+    for (i = 0; i < d; i++)
+    {
+        float val = 0.0f;
+        for (int j = 0; j < n; j++)
+        {
+            val += w[i * n + j] * x[j];
+        }
+        xout[i] = val;
+    }
+}
+
+static float *forward(Transformer *transformer, int token, int pos)
+{
+
+    // a few convenience variables
+    Config *p = &transformer->config;
+    TransformerWeights *w = &transformer->weights;
+    RunState *s = &transformer->state;
+    float *x = s->x;
+    int dim = p->dim;
+    int kv_dim = (p->dim * p->n_kv_heads) / p->n_heads;
+    int kv_mul = p->n_heads / p->n_kv_heads; // integer multiplier of the kv sharing in multiquery
+    int hidden_dim = p->hidden_dim;
+    int head_size = dim / p->n_heads;
+
+    // copy the token embedding into x
+    float *content_row = w->token_embedding_table + token * dim;
+    memcpy(x, content_row, dim * sizeof(*x));
+
+    // forward all the layers
+    for (unsigned long long l = 0; l < p->n_layers; l++)
+    {
+
+        // attention rmsnorm
+        rmsnorm(s->xb, x, w->rms_att_weight + l * dim, dim);
+
+        // key and value point to the kv cache
+        int loff = l * p->seq_len * kv_dim; // kv cache layer offset for convenience
+        s->k = s->key_cache + loff + pos * kv_dim;
+        s->v = s->value_cache + loff + pos * kv_dim;
+
+        // qkv matmuls for this position
+        matmul(s->q, s->xb, w->wq + l * dim * dim, dim, dim);
+        matmul(s->k, s->xb, w->wk + l * dim * kv_dim, dim, kv_dim);
+        matmul(s->v, s->xb, w->wv + l * dim * kv_dim, dim, kv_dim);
+
+        // RoPE relative positional encoding: complex-valued rotate q and k in each head
+        for (int i = 0; i < dim; i += 2)
+        {
+            int head_dim = i % head_size;
+            float freq = 1.0f / powf(10000.0f, head_dim / (float)head_size);
+            float val = pos * freq;
+            float fcr = cosf(val);
+            float fci = sinf(val);
+            int rotn = i < kv_dim ? 2 : 1; // how many vectors? 2 = q & k, 1 = q only
+            for (int v = 0; v < rotn; v++)
+            {
+                float *vec = v == 0 ? s->q : s->k; // the vector to rotate (query or key)
+                float v0 = vec[i];
+                float v1 = vec[i + 1];
+                vec[i] = v0 * fcr - v1 * fci;
+                vec[i + 1] = v0 * fci + v1 * fcr;
+            }
+        }
+
+        // multihead attention. iterate over all heads
+        int h;
+#pragma omp parallel for private(h)
+        for (h = 0; h < p->n_heads; h++)
+        {
+            // get the query vector for this head
+            float *q = s->q + h * head_size;
+            // attention scores for this head
+            float *att = s->att + h * p->seq_len;
+            // iterate over all timesteps, including the current one
+            for (int t = 0; t <= pos; t++)
+            {
+                // get the key vector for this head and at this timestep
+                float *k = s->key_cache + loff + t * kv_dim + (h / kv_mul) * head_size;
+                // calculate the attention score as the dot product of q and k
+                float score = 0.0f;
+                for (int i = 0; i < head_size; i++)
+                {
+                    score += q[i] * k[i];
+                }
+                score /= sqrtf(head_size);
+                // save the score to the attention buffer
+                att[t] = score;
+            }
+
+            // softmax the scores to get attention weights, from 0..pos inclusively
+            softmax(att, pos + 1);
+
+            // weighted sum of the values, store back into xb
+            float *xb = s->xb + h * head_size;
+            memset(xb, 0, head_size * sizeof(float));
+            for (int t = 0; t <= pos; t++)
+            {
+                // get the value vector for this head and at this timestep
+                float *v = s->value_cache + loff + t * kv_dim + (h / kv_mul) * head_size;
+                // get the attention weight for this timestep
+                float a = att[t];
+                // accumulate the weighted value into xb
+                for (int i = 0; i < head_size; i++)
+                {
+                    xb[i] += a * v[i];
+                }
+            }
+        }
+
+        // final matmul to get the output of the attention
+        matmul(s->xb2, s->xb, w->wo + l * dim * dim, dim, dim);
+
+        // residual connection back into x
+        for (int i = 0; i < dim; i++)
+        {
+            x[i] += s->xb2[i];
+        }
+
+        // ffn rmsnorm
+        rmsnorm(s->xb, x, w->rms_ffn_weight + l * dim, dim);
+
+        // Now for FFN in PyTorch we have: self.w2(F.silu(self.w1(x)) * self.w3(x))
+        // first calculate self.w1(x) and self.w3(x)
+        matmul(s->hb, s->xb, w->w1 + l * dim * hidden_dim, dim, hidden_dim);
+        matmul(s->hb2, s->xb, w->w3 + l * dim * hidden_dim, dim, hidden_dim);
+
+        // SwiGLU non-linearity
+        for (int i = 0; i < hidden_dim; i++)
+        {
+            float val = s->hb[i];
+            // silu(x)=x*σ(x), where σ(x) is the logistic sigmoid
+            val *= (1.0f / (1.0f + expf(-val)));
+            // elementwise multiply with w3(x)
+            val *= s->hb2[i];
+            s->hb[i] = val;
+        }
+
+        // final matmul to get the output of the ffn
+        matmul(s->xb, s->hb, w->w2 + l * dim * hidden_dim, hidden_dim, dim);
+
+        // residual connection
+        for (int i = 0; i < dim; i++)
+        {
+            x[i] += s->xb[i];
+        }
+    }
+
+    // final rmsnorm
+    rmsnorm(x, x, w->rms_final_weight, dim);
+
+    // classifier into logits
+    matmul(s->logits, x, w->wcls, p->dim, p->vocab_size);
+    return s->logits;
+}
+
+static int sample_argmax(float *probabilities, int n)
+{
+    // return the index that has the highest probability
+    int max_i = 0;
+    float max_p = probabilities[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (probabilities[i] > max_p)
+        {
+            max_i = i;
+            max_p = probabilities[i];
+        }
+    }
+    return max_i;
+}
+
+static int sample_mult(float *probabilities, int n, float coin)
+{
+    // sample index from probabilities (they must sum to 1!)
+    // coin is a random number in [0, 1), usually from random_f32()
+    float cdf = 0.0f;
+    for (int i = 0; i < n; i++)
+    {
+        cdf += probabilities[i];
+        if (coin < cdf)
+        {
+            return i;
+        }
+    }
+    return n - 1; // in case of rounding errors
+}
+
+static int compare(const void *a, const void *b)
+{
+    ProbIndex *a_ = (ProbIndex *)a;
+    ProbIndex *b_ = (ProbIndex *)b;
+    if (a_->prob > b_->prob)
+        return -1;
+    if (a_->prob < b_->prob)
+        return 1;
+    return 0;
+}
+
+static int sample_topp(float *probabilities, int n, float topp, ProbIndex *probindex, float coin)
+{
+    // top-p sampling (or "nucleus sampling") samples from the smallest set of
+    // tokens that exceed probability topp. This way we never sample tokens that
+    // have very low probabilities and are less likely to go "off the rails".
+    // coin is a random number in [0, 1), usually from random_f32()
+
+    int n0 = 0;
+    // quicksort indices in descending order of probabilities
+    // values smaller than (1 - topp) / (n - 1) cannot be part of the result
+    // so for efficiency we crop these out as candidates before sorting
+    const float cutoff = (1.0f - topp) / (n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        if (probabilities[i] >= cutoff)
+        {
+            probindex[n0].index = i;
+            probindex[n0].prob = probabilities[i];
+            n0++;
+        }
+    }
+    qsort(probindex, n0, sizeof(ProbIndex), compare);
+
+    // truncate the list where cumulative probability exceeds topp
+    float cumulative_prob = 0.0f;
+    int last_idx = n0 - 1; // in case of rounding errors consider all elements
+    for (int i = 0; i < n0; i++)
+    {
+        cumulative_prob += probindex[i].prob;
+        if (cumulative_prob > topp)
+        {
+            last_idx = i;
+            break; // we've exceeded topp by including last_idx
+        }
+    }
+
+    // sample from the truncated list
+    float r = coin * cumulative_prob;
+    float cdf = 0.0f;
+    for (int i = 0; i <= last_idx; i++)
+    {
+        cdf += probindex[i].prob;
+        if (r < cdf)
+        {
+            return probindex[i].index;
+        }
+    }
+    return probindex[last_idx].index; // in case of rounding errors
+}
+
+static float random_f32(unsigned long long *state)
+{ // random float32 in [0,1)
+    return (random_u32(state) >> 8) / 16777216.0f;
+}
+
+static int sample(Sampler *sampler, float *logits)
+{
+    // sample the token given the logits and some hyperparameters
+    int next;
+    if (sampler->temperature == 0.0f)
+    {
+        // greedy argmax sampling: take the token with the highest probability
+        next = sample_argmax(logits, sampler->vocab_size);
+    }
+    else
+    {
+        // apply the temperature to the logits
+        for (int q = 0; q < sampler->vocab_size; q++)
+        {
+            logits[q] /= sampler->temperature;
+        }
+        // apply softmax to the logits to get the probabilities for next token
+        softmax(logits, sampler->vocab_size);
+        // flip a (float) coin (this is our source of entropy for sampling)
+        float coin = random_f32(&sampler->rng_state);
+        // we sample from this distribution to get the next token
+        if (sampler->topp <= 0 || sampler->topp >= 1)
+        {
+            // simply sample from the predicted probability distribution
+            next = sample_mult(logits, sampler->vocab_size, coin);
+        }
+        else
+        {
+            // top-p (nucleus) sampling, clamping the least likely tokens to zero
+            next = sample_topp(logits, sampler->vocab_size, sampler->topp, sampler->probindex, coin);
+        }
+    }
+    return next;
+}
+
+static char *decode(Tokenizer *t, int prev_token, int token)
+{
+    char *piece = t->vocab[token];
+    // following BOS (1) token, sentencepiece decoder strips any leading whitespace (see PR #89)
+    if (prev_token == 1 && piece[0] == ' ')
+    {
+        piece++;
+    }
+    // careful, some tokens designate raw bytes, and look like e.g. '<0x01>'
+    // parse this and convert and return the actual byte
+    unsigned char byte_val;
+    if (sscanf(piece, "<0x%02hhX>", &byte_val) == 1)
+    {
+        piece = (char *)t->byte_pieces + byte_val * 2;
+    }
+    return piece;
+}
+
+void cluster(Transformer *transformer,
+             Tokenizer *tokenizer,
+             Sampler *sampler,
+             int *next,
+             int *pos,
+             int token,
+             int *prompt_tokens,
+             int num_prompt_tokens,
+             bool *must_break)
+{
+    // forward the transformer to get logits for the next token
+    float *logits = forward(transformer, token, *pos);
+
+    // advance the state machine
+    if (*pos < num_prompt_tokens - 1)
+    {
+        // if we are still processing the input prompt, force the next prompt token
+        *next = prompt_tokens[*pos + 1];
+    }
+    else
+    {
+        // otherwise sample the next token from the logits
+        *next = sample(sampler, logits);
+    }
+    (*pos)++;
+
+    // data-dependent terminating condition: the BOS (=1) token delimits sequences
+    if (next == 1)
+    {
+        *must_break = true;
+        return;
+    }
+
+    // print the token as string, decode it with the Tokenizer object
+    char *piece = decode(tokenizer, token, *next);
+}
