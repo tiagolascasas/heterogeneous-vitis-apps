349390cc276ebdc578102f4e62f2c7e51a12e8bb
tiagolascasas <tiagolascasas@outlook.com>
2025-03-14 23:59:05 +0000

    Adds an exploratory project to experiment with HLS streams

---
+++ exploration-streams/vadd-streams/_ide/workspace_journal.py
@@ -0,0 +1,8 @@
+# 2025-03-14T23:57:03.625950
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="vadd-streams")
+
+proj = client.create_sys_project(name="vadd", platform="/tools/Xilinx/Vitis/2024.2/base_platforms/xilinx_zcu102_base_202420_1/xilinx_zcu102_base_202420_1.xpfm", template="installed_examples/vadd")
+
